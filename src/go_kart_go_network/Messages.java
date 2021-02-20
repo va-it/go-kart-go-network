72251f57909b9a446f574458ef2c2100f7648eaf
Vincenzo Abete <abetevincenzo@gmail.com>
2021-02-20 16:59:10 +0000

    Made messages to be constants

--- src/go_kart_go_network/Messages.java
+++ src/go_kart_go_network/Messages.java
@@ -1,18 +1,18 @@
 package go_kart_go_network;
 
 public class Messages {
-    public static String establishConnection = "establish_connection";
-    public static String connectionSuccessful = "connection_successful";
-    public static String getPlayerNumber = "get_player_number";
-    public static String startRace = "start_race";
-    public static String stopRace = "stop_race";
-    public static String closeConnection = "close_connection";
-    public static String sendingKartInfo = "sending_kart_info";
-    public static String kartInfoReceived = "kart_info_received";
-    public static String getOpponentIndex = "get_opponent_index";
-    public static String getOpponentSpeed = "get_opponent_speed";
+    public static final String establishConnection = "establish_connection";
+    public static final String connectionSuccessful = "connection_successful";
+    public static final String getPlayerNumber = "get_player_number";
+    public static final String startRace = "start_race";
+    public static final String stopRace = "stop_race";
+    public static final String closeConnection = "close_connection";
+    public static final String sendingKartInfo = "sending_kart_info";
+    // public static String kartInfoReceived = "kart_info_received";
+    public static final String getOpponentIndex = "get_opponent_index";
+    public static final String getOpponentSpeed = "get_opponent_speed";
 
-    public static String returnPlayerNumber(int player) { return String.valueOf(player); }
-    public static String returnSpeed(int speed) { return String.valueOf(speed); }
-    public static String returnIndex(int index) { return String.valueOf(index); }
+    public static final String returnPlayerNumber(int player) { return String.valueOf(player); }
+    public static final String returnSpeed(int speed) { return String.valueOf(speed); }
+    public static final String returnIndex(int index) { return String.valueOf(index); }
 }
